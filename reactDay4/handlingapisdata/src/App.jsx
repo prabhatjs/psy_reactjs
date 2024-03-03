e1295e2cd69f34183d0f588dd37712e6de2162be
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-03-03 18:25:53 +0530

    Add new Function handle api and condition renders

--- reactDay4/handlingapisdata/src/App.jsx
+++ reactDay4/handlingapisdata/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import './App.css'
 import Fetchapi from './api/Fetchapi'
 import Productapi from './api/Productapi';
+import Transaction from './api/TransactionHistory';
+import EffectHook from './components/EffectHook';
 
 function App() {
   //create async function to handle api data,async function 
   const [product,setProduct]=useState([]);
+  const [Transactions,setTransaction]=useState([]);
+  const [highlighted,setHighlighted]=useState(false);
   //print on console
   const handleclick =async()=>{
     try {
@@ -30,11 +34,40 @@ function App() {
     }
   }
   //print on page and conditon render 
+  const handleTransaction=async()=>{
+    try {
+      const {data,status}=await Transaction("http://transaction.com/api/transactions");
+      if(status===200){
+        setTransaction(data.Transaction);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function highLightTansaction(){
+    setHighlighted(true);
+  }
   return (
     <>
-    <h1>City Data</h1>
       <button onClick={handleclick}>Get City Data</button>
       <button onClick={handleclickShowonPage}>Get Product Data</button>
+      <button onClick={handleTransaction}>Transaction Data</button>
+      <button onClick={highLightTansaction}>High Light Transactions</button>
+      <div>
+      <ul style={{listStyle:'none'}}>
+        {Transactions.map(({Amount,Date,platform})=>{
+          return(
+            <li style={{color:Amount>5000?'red':'blue', border:highlighted && Amount>5000 ? '2px solid Yellow':'none'}}>
+              <p>Amount:{Amount}</p>
+              <p>Date:{Date}</p>
+              <p>Application:{platform}</p>
+            </li>
+          )
+        })}
+      </ul>
+      </div>
+      <div>
       <ul style={{listStyle:'none'}}>
         {product.map((item)=>{
          return (
@@ -45,6 +78,8 @@ function App() {
          )
         })}
       </ul>
+      </div>
+      <EffectHook/>
     </>
   )
 }
