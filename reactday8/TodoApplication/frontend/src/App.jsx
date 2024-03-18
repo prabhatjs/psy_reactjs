f964ee669506dfeb19813a4a4f54409322f8d27b
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-03-18 21:28:52 +0530

    Add fronet end and logic of it how to send data from frontend

--- reactday8/TodoApplication/frontend/src/App.jsx
+++ reactday8/TodoApplication/frontend/src/App.jsx
@@ -1,17 +1,26 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import CreateTodo from './components/CreateTodo'
 import Todos from './components/Todos'
 function App() {
 const [todos,settodo]=useState([]);
 
-  fetch("http://localhost:8000/api/todo")
+  // fetch("http://localhost:8000/api/todo")
+  // .then(async function(res){
+  //   const data=await res.json();
+  //   console.log(data);
+  //   settodo(data.todos);
+  // })
+
+  useEffect(()=>{
+    fetch("http://localhost:8000/api/todo")
   .then(async function(res){
     const data=await res.json();
     console.log(data);
     settodo(data.todos);
   })
+  },[])
   return (
     <>
       <CreateTodo></CreateTodo>
--- reactday8/TodoApplication/frontend/src/components/CreateTodo.jsx
+++ reactday8/TodoApplication/frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateTodo() {
+    const [title,setTitle]=useState("");
+    const [description,setdescription]=useState('');
+
+    function handletitle(event)
+    {
+        setTitle(event.target.value);
+    }
+    function handledescription(event){
+        setdescription(event.target.value);
+    }
+    function addtodo(){
+        fetch('http://localhost:8000/api/todo',{
+            method:'POST',
+            body:JSON.stringify({
+                title:title,
+                description:description
+            }),
+            headers:{
+                "Content-type":"application/json"
+            }
+        }).then(async function(res){
+            const data=await res.json();
+            alert("Todo Added")
+        })
+    }
+
   return (
     <div>
-        <input type='text' placeholder='title'/><br/>
-        <input type='text' placeholder='description'/><br/>
-        <button>Add Todo</button>
+        <input type='text' placeholder='title' onChange={handletitle}/><br/>
+        <input type='text' placeholder='description' onChange={handledescription}/><br/>
+        <button onClick={addtodo}>Add Todo</button>
     </div>
   )
 }
